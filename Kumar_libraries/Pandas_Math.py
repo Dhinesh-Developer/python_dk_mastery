053535f062dfc09e1a9e5e58441d0e4c15f61827
Dhinesh-Developer <dhineshdeveloper07@gmail.com>
2026-03-29 11:50:52 +0530

    Modules VS oops part 1

---
+++ Kumar_libraries/Pandas_Math.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def addition(a,b):
+    return a+b
+
+def subtraction(a,b):
+    return a-b
+
+def multiplication(x,y):
+    return x*y
+
+    
+
+
+# ### Terminology Alert
+# - Parameters - While creating the function ,you create paramters
+# - Attributes - while Calling the function, you pass arguments
+
+# # Difference Between Return and print
+# - Return - impacts the original Data when the function is called
+# - Print - it wont impact the Original Data, but just prints
+
+# In[ ]:
+
+
+
+
